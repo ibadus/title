b638b7445a2ac7ba70c3f08b806cfcd9fec5f918
ibadus <ibadus@icloud.com>
2023-01-14 22:45:02 +0100

    first commit

---
+++ title.go
@@ -0,0 +1,34 @@
+package title
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+// Set a title on the terminal for macos and windows
+func Set(title string) {
+	if runtime.GOOS == "darwin" {
+		cmd := exec.Command("echo", "-n", "\033]0;"+title+"\007")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Start()
+		if err != nil {
+			return
+		}
+		err = cmd.Wait()
+		if err != nil {
+			return
+		}
+	} else {
+		// set the title of the terminal
+		cmd := exec.Command("cmd", "/C", "title", title)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return
+		}
+	}
+}
---
+++ title_test.go
@@ -0,0 +1,10 @@
+package title_test
+
+import (
+	"testing"
+	"title"
+)
+
+func TestSet(t *testing.T) {
+	title.Set("testing title")
+}
